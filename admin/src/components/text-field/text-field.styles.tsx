e698ae21e15894ed3429f6ba0f5a72f57c5a0ad0
apple <apple@iMac-apple-2.local>
2021-06-17 17:20:16 +0300

    feature/text-field

---
+++ admin/src/components/text-field/text-field.styles.tsx
@@ -0,0 +1,67 @@
+import styled, { css } from 'styled-components';
+
+import { COLORS } from '@styles/colors';
+import { FONT_SIZES } from '@styles/font-sizes';
+
+import { ITextFieldStylesProps } from './text-field.typings';
+
+const textFieldDisabled = css`
+  opacity: 0.5;
+`;
+
+const textFieldState = css`
+  :hover,
+  :focus,
+  :focus-visible {
+    border: 2px solid ${COLORS.primary};
+    outline-color: ${COLORS.primary};
+  }
+`;
+
+export const TextFieldWrapper = styled.div<ITextFieldStylesProps>`
+  position: relative;
+  ${({ disabled }) => disabled && textFieldDisabled};
+
+  input,
+  textarea {
+    padding: 10px;
+    color: ${COLORS.default};
+    font-size: ${FONT_SIZES.default};
+    font-family: inherit;
+    border-radius: 5px;
+    resize: none;
+    border: 2px solid ${({ error }) => (error ? COLORS.red : COLORS.default)};
+    width: ${({ isFullWidth, width }) =>
+      isFullWidth ? '100%' : width || '200px'};
+    height: ${({ height }) => height || 'auto'};
+
+    ${({ disabled }) => (disabled ? `cursor: not-allowed` : textFieldState)};
+  }
+
+  input:focus + label,
+  textarea:focus + label {
+    top: -12px;
+  }
+
+  label {
+    position: absolute;
+    background-color: ${COLORS.white};
+    top: 10px;
+    left: 10px;
+    padding: 2px;
+    transition: 0.3s;
+
+    ${({ value }) => value && `top: -12px`}
+  }
+`;
+
+export const ErrorBlock = styled.div<ITextFieldStylesProps>`
+  position: absolute;
+  width: ${({ isFullWidth, width }) =>
+    isFullWidth ? '100%' : width || '200px'};
+  padding: 0 10px;
+  color: ${COLORS.red};
+  font-size: 0.7em;
+  border-radius: 5px;
+  z-index: 1;
+`;
